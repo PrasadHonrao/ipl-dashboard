f67fa14e9e1a21ec36ce4c70bd2bea78f904b025
Prasad Honrao <Honrao.Prasad@Gmail.com>
2022-03-12 17:48:53 +0000

    Enable CORS

--- api/java/ipl-dashboard/src/main/java/com/ph/ipldashboard/controllers/TeamController.java
+++ api/java/ipl-dashboard/src/main/java/com/ph/ipldashboard/controllers/TeamController.java
@@ -4,11 +4,13 @@ import com.ph.ipldashboard.models.Team;
 import com.ph.ipldashboard.repositories.MatchRepository;
 import com.ph.ipldashboard.repositories.TeamRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class TeamController {
 
     TeamRepository teamRepository;
